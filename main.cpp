c13262cfdb492d7e4294b10e892bebd6e7b7d3d3
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-01 18:07:48 +0300

    test for matrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include "Matrix.h"
+
+using namespace std;
+
+// test addition of two matrices
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Matrix m1(3, 3);
+    Matrix m2(3, 3);
+
+    double** data1 = new double*[9];
+    double** data2 = new double*[9];
+
+    for (int i = 0; i < 9; i++) {
+        data1[i] = new double[9];
+        data2[i] = new double[9];
+        for (int j = 0; j < 9; j++) {
+            data1[i][j] = i + j;
+            data2[i][j] = i + j;
+        }
+    }
+
+    m1.setValues(data1);
+    m2.setValues(data2);
+
+    Matrix m3 = m1 + m2;
+    m3.print();
+    m3 = m1;
+    m3.print();
+
+    m3 + m2;
+    m3.print();
+
     return 0;
+
+
 }
